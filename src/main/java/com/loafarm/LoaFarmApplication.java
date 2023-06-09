a17fa4108a6e6a77bb274901450d9c708288b443
DESKTOP-DGKU219\User <qhdms136@naver.com>
2023-06-09 19:18:01 +0900

    aws 파일 경로 / 개수 수정

--- src/main/java/com/loafarm/LoaFarmApplication.java
+++ src/main/java/com/loafarm/LoaFarmApplication.java
@@ -2,9 +2,10 @@ package com.loafarm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LoaFarmApplication {
+public class LoaFarmApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoaFarmApplication.class, args);
